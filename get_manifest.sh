193efe11c8f9de91f41f3d035d97436838d8f99a
Richard Newton <therichardnewton+github@gmail.com>
2022-07-27 10:46:28 +0100

    Download Files
    Updated get_manifest to just download files and nothing else.

--- get_manifest.sh
+++ get_manifest.sh
@@ -16,20 +16,14 @@ device=TiVoAdvert
 if [[ $env == prod ]]
 then
   # prod
-#  server=$(grep ${env}_server get_manifest.conf | cut -d= -f2)
-  server="localhost"
   ms=$(grep ${env}_ms get_manifest.conf | cut -d= -f2)
 elif [[ $env == preprod ]]
 then
   # preprod
-#  server=$(grep ${env}_server get_manifest.conf | cut -d= -f2)
-  server="localhost"
   ms=$(grep ${env}_ms get_manifest.conf | cut -d= -f2)
 elif [[ $env == ft ]]
 then
   # ft
-#  server=$(grep ${env}_server get_manifest.conf | cut -d= -f2)
-  server="localhost"
   ms=$(grep ${env}_ms get_manifest.conf | cut -d= -f2)
 else
   echo "You done goofed, boy!"
@@ -38,15 +32,5 @@ fi
 echo "Remeber to use ssh ppfe (or whatever) to make the connection to Traxis"
 echo "Gotta love VPN changes that break things..."
 
-name=$(curl -s "http://${server}:8443/traxis/web/Title/crid:~~2F~~2Fschange.com~~2F${provider_id}~~2F${resource_id}/Props/Name" | grep Name | cut -d\> -f2 | cut -d\< -f1)
-details=$(curl -s "http://${server}:8443/traxis/web/Title/crid:~~2F~~2Fschange.com~~2F${provider_id}~~2F${resource_id}/Contents/Props/Aliases" | tr -d '\r')
-backoffice_id=$(echo $details | grep VodBackOfficeId | cut -d\> -f6- | cut -d\< -f1)
-breakpoints=$(echo $details | grep "Genre:other" | cut -d\> -f10- | cut -d\< -f1)
-echo Programme = $name 
-echo Breakpoints = $breakpoints
-echo
-echo Manifest
-echo ========
-echo
 #
-http --body  --follow "http://${ms}/sdash/${backoffice_id}/index.mpd/Manifest?providerID=${provider_id}&assetID=${resource_id}&macid=${mac_id}&device=${device}"
+http --body  --follow "http://${ms}/sdash/${backoffice_id}/index.mpd/Manifest?providerID=${provider_id}&assetID=${resource_id}&macid=${mac_id}&device=${device}" --output manifest.xml --download
---
+++ show_manifest_and_breakpoints.sh
@@ -0,0 +1,52 @@
+#! /bin/bash
+
+# Asset Details
+provider_id=CHA
+#resource_id=TITL0000000001827829
+resource_id=TITL0200000004553465
+#env=prod
+env=preprod
+#env=ft
+
+# General Details
+mac_id=aa:bb:cc:dd:ee:ff
+device=TiVoAdvert
+
+# Environment Details
+if [[ $env == prod ]]
+then
+  # prod
+#  server=$(grep ${env}_server get_manifest.conf | cut -d= -f2)
+  server="localhost"
+  ms=$(grep ${env}_ms get_manifest.conf | cut -d= -f2)
+elif [[ $env == preprod ]]
+then
+  # preprod
+#  server=$(grep ${env}_server get_manifest.conf | cut -d= -f2)
+  server="localhost"
+  ms=$(grep ${env}_ms get_manifest.conf | cut -d= -f2)
+elif [[ $env == ft ]]
+then
+  # ft
+#  server=$(grep ${env}_server get_manifest.conf | cut -d= -f2)
+  server="localhost"
+  ms=$(grep ${env}_ms get_manifest.conf | cut -d= -f2)
+else
+  echo "You done goofed, boy!"
+fi
+
+echo "Remeber to use ssh ppfe (or whatever) to make the connection to Traxis"
+echo "Gotta love VPN changes that break things..."
+
+name=$(curl -s "http://${server}:8443/traxis/web/Title/crid:~~2F~~2Fschange.com~~2F${provider_id}~~2F${resource_id}/Props/Name" | grep Name | cut -d\> -f2 | cut -d\< -f1)
+details=$(curl -s "http://${server}:8443/traxis/web/Title/crid:~~2F~~2Fschange.com~~2F${provider_id}~~2F${resource_id}/Contents/Props/Aliases" | tr -d '\r')
+backoffice_id=$(echo $details | grep VodBackOfficeId | cut -d\> -f6- | cut -d\< -f1)
+breakpoints=$(echo $details | grep "Genre:other" | cut -d\> -f10- | cut -d\< -f1)
+echo Programme = $name 
+echo Breakpoints = $breakpoints
+echo
+echo Manifest
+echo ========
+echo
+#
+http --body  --follow "http://${ms}/sdash/${backoffice_id}/index.mpd/Manifest?providerID=${provider_id}&assetID=${resource_id}&macid=${mac_id}&device=${device}"
