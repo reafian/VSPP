17851fb52442f26584f9f61f083fd3018e319449
Richard Newton <therichardnewton+github@gmail.com>
2022-07-27 09:31:53 +0100

    Update get_manifest.sh
    corrected server and ms variables so they work. Tweaked server to work with ssh.

--- get_manifest.sh
+++ get_manifest.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 
 # Asset Details
-provider_id=V4K
+provider_id=CHA
 #resource_id=TITL0000000001827829
-resource_id=TITL0100000000028811
-env=prod
-#env=preprod
+resource_id=TITL0200000004553465
+#env=prod
+env=preprod
 #env=ft
 
 # General Details
@@ -16,29 +16,37 @@ device=TiVoAdvert
 if [[ $env == prod ]]
 then
   # prod
-  server=$(grep $prod_server get_manifest.conf | cut -d= -f2)
-  ms=$(grep $prod_ms get_manifest.conf | cut -d= -f2)
+#  server=$(grep ${env}_server get_manifest.conf | cut -d= -f2)
+  server="localhost"
+  ms=$(grep ${env}_ms get_manifest.conf | cut -d= -f2)
 elif [[ $env == preprod ]]
 then
   # preprod
-  server=$(grep $preprod_server get_manifest.conf | cut -d= -f2)
-  ms=$(grep $preprod_ms get_manifest.conf | cut -d= -f2)
+#  server=$(grep ${env}_server get_manifest.conf | cut -d= -f2)
+  server="localhost"
+  ms=$(grep ${env}_ms get_manifest.conf | cut -d= -f2)
 elif [[ $env == ft ]]
 then
   # ft
-  server=$(grep $ft_server get_manifest.conf | cut -d= -f2)
-  ms=$(grep $ft_ms get_manifest.conf | cut -d= -f2)
+#  server=$(grep ${env}_server get_manifest.conf | cut -d= -f2)
+  server="localhost"
+  ms=$(grep ${env}_ms get_manifest.conf | cut -d= -f2)
 else
   echo "You done goofed, boy!"
 fi
 
+echo "Remeber to use ssh ppfe (or whatever) to make the connection to Traxis"
+echo "Gotta love VPN changes that break things..."
+
 name=$(curl -s "http://${server}:8443/traxis/web/Title/crid:~~2F~~2Fschange.com~~2F${provider_id}~~2F${resource_id}/Props/Name" | grep Name | cut -d\> -f2 | cut -d\< -f1)
 details=$(curl -s "http://${server}:8443/traxis/web/Title/crid:~~2F~~2Fschange.com~~2F${provider_id}~~2F${resource_id}/Contents/Props/Aliases" | tr -d '\r')
 backoffice_id=$(echo $details | grep VodBackOfficeId | cut -d\> -f6- | cut -d\< -f1)
-
-http --body  --follow "http://${ms}/sdash/${backoffice_id}/index.mpd/Manifest?providerID=${provider_id}&assetID=${resource_id}&macid=${mac_id}&device=${device}" --output manifest.xml --download
-#http --body  --follow "http://${ms}/sdash/${backoffice_id}/index.mpd/Manifest?providerID=${provider_id}&assetID=${resource_id}&macid=${mac_id}&device=${device}"
-
-#echo "http://${ms}/sdash/${backoffice_id}/index.mpd/Manifest?providerID=${provider_id}&assetID=${resource_id}&macid=${mac_id}&device=${device}"
-# More generic, less pretty version
-#curl -L "http://multiscreen-dash.vod.mspp.dtv.virginmedia.com/sdash/${backoffice_id}/index.mpd/Manifest?providerID=${provider_id}&assetID=${resource_id}&macid=${mac_id}&device=${device}"
+breakpoints=$(echo $details | grep "Genre:other" | cut -d\> -f10- | cut -d\< -f1)
+echo Programme = $name 
+echo Breakpoints = $breakpoints
+echo
+echo Manifest
+echo ========
+echo
+#
+http --body  --follow "http://multiscreen-dash.vod.mspp.dtv.virginmedia.com/sdash/${backoffice_id}/index.mpd/Manifest?providerID=${provider_id}&assetID=${resource_id}&macid=${mac_id}&device=${device}"
