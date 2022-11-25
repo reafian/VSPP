6ba121d9da80f1acfbee823a33937e23a84f79e1
Richard Newton <therichardnewton+github@gmail.com>
2022-11-25 12:57:22 +0000

    Update of extract.py
    Renamed so it makes more sense. Massive re-write to be a bit more functiony. Still need to fix the actual extraction though.
    
    Baked in the ffprobe command to manage the necessary arguments better.

---
+++ GOPInfo.py
@@ -0,0 +1,91 @@
+#! /usr/bin/python3
+import subprocess
+import argparse
+import shutil
+import json
+
+# Use argparses to generate useful help messages
+parser = argparse.ArgumentParser()
+parser.add_argument("file", help="Use \"GOPInfo <file>\" to extract GOP information.")
+args = parser.parse_args()
+
+# Create filenames for writing
+json_file=args.file.split(".")[0] + ".json"
+text_file=args.file.split(".")[0] + ".txt"
+
+# main function that calls the rest
+def main():
+	probe_result = call_ffprobe(args.file)
+	extract_frames(probe_result)
+
+# funtion to mange ffprobe
+def call_ffprobe(file):
+	ffprobe = is_program_installed("ffprobe")
+	if ffprobe == None:
+		sys.exit("ffprobe not found!")
+	else:
+		print("Running ffprobe on file.")
+		probe = run_ffprobe(ffprobe, file)
+		return(probe)
+
+# function to check if ffprobe is installed or not
+def is_program_installed(ffprobe):
+	return shutil.which(ffprobe)
+
+# function to run ffprobe and generate a file to work with
+# I'm not sure if we really need to write the file out (will size play an issue?)
+# but I'm doing it anyway so I have a reference for later.
+def run_ffprobe(ffprobe, file):
+	with open(json_file, "w") as jf:
+		result = subprocess.Popen([ffprobe, "-select_streams", "v", "-show_frames", "-show_entries", 
+			"frame=pts,pkt_duration,pkt_pos,pict_type,pkt_size", "-v", "quiet", "-print_format", 
+			"json", file], universal_newlines=True, stdout=subprocess.PIPE).communicate()[0]
+		jf.write(str(result))
+	return(result)
+
+def extract_frames(probe_result):
+	filedata = json.loads(probe_result)
+	# Empty frames array
+	frames = []
+	for properties in filedata['frames']:
+#		print(properties)
+		for key, value in properties.items():
+
+			# Find the pts values for each frame
+			if key == "pts":
+				pts = value
+
+			# Find the type of each frame
+			if key == "pict_type":
+				pict_type = value.strip()
+
+				# If the frame is an I-Frame then we need to do something
+				if pict_type == "I":
+					pict_type = " I"
+
+					# Zero out both the GOP and the count values
+					gop_line = ""
+					count = 0
+
+					# Add the frame to the GOP and increment the line count by one
+					for frame in frames:
+						gop_line += str(frame)
+						count += 1
+
+					if len(gop_line) > 1:
+						gop_result = str(count-1) + " " + gop_line
+						print(gop_result)
+						with open(text_file, 'a') as output:
+							output.write(gop_result+"\n")
+					
+					# Need to zero out the array each time so I-Frames start on new lines
+					# Zero the array, add the pts and the I-Frame
+					frames = []
+					frames.append(pts)
+					frames.append(pict_type)
+				else:
+					# If the frame is not an I-Frame then tag it onto the end
+					frames.append(pict_type)
+
+
+main()
