a24b4c70aace6aec1e03c0afc69700e0c6c891f6
Richard Newton <therichardnewton+github@gmail.com>
2022-11-29 21:20:26 +0000

    Update GOPInfo.py
    Added step to remove old .txt file.
    Tweaked routine to determine time values. Appear to have fixed issue with an incorrect GOP being reported.

--- GOPInfo.py
+++ GOPInfo.py
@@ -4,15 +4,22 @@ import argparse
 import shutil
 import json
 import time
+import os
 
 # Use argparses to generate useful help messages
 parser = argparse.ArgumentParser()
 parser.add_argument("file", help="Use \"GOPInfo <file>\" to extract GOP information.")
 args = parser.parse_args()
 
+first_iframe = 0
+pts_offset = 0
+
 # Create filenames for writing
 json_file=args.file.split(".")[0] + ".json"
 text_file=args.file.split(".")[0] + ".txt"
+
+if os.path.exists(text_file):
+	os.remove(text_file)
 
 # main function that calls the rest
 def main():
@@ -37,9 +44,6 @@ def is_program_installed(ffprobe):
 # I'm not sure if we really need to write the file out (will size play an issue?)
 # but I'm doing it anyway so I have a reference for later.
 def run_ffprobe(ffprobe, file):
-#	print(ffprobe)
-#	print(file)
-#	print(json_file)
 	with open(json_file, "w") as jf:
 		result = subprocess.Popen([ffprobe, "-select_streams", "v", "-show_frames", "-show_entries", 
 			"frame=pts,pkt_duration,pkt_pos,pict_type,pkt_size,pkt_dts_time", "-v", "quiet", "-print_format", 
@@ -47,9 +51,13 @@ def run_ffprobe(ffprobe, file):
 		jf.write(str(result))
 	return(result)
 
-def convert_time(pkt_dts_time):
-	return(time.strftime("%H:%M:%S", time.gmtime(float(pkt_dts_time))))
-#	return(sec)
+def real_time(pkt_dts_time, offset):
+	time_difference = float(pkt_dts_time) - float(pts_offset)
+	return(time.strftime("%H:%M:%S", time.gmtime(float(time_difference))))
+
+def get_offset(pkt_dts_time):
+	if first_iframe == 0:
+		return pkt_dts_time
 
 def extract_frames(probe_result):
 	print("Extracting GOP structure of file.")
@@ -57,7 +65,6 @@ def extract_frames(probe_result):
 	# Empty frames array
 	frames = []
 	for properties in filedata['frames']:
-#		print(properties)
 		for key, value in properties.items():
 
 			# Find the pts values for each frame
@@ -65,7 +72,7 @@ def extract_frames(probe_result):
 				pts = value
 
 			if key == "pkt_dts_time":
-				pkt_dts_time = " " + convert_time(value)
+				pkt_dts_time = value
 
 			# Find the type of each frame
 			if key == "pict_type":
@@ -74,6 +81,15 @@ def extract_frames(probe_result):
 				# If the frame is an I-Frame then we need to do something
 				if pict_type == "I":
 					pict_type = " I"
+					
+					global pts_offset
+					if pts_offset == 0:
+						pts_offset = get_offset(pkt_dts_time)
+
+					global first_iframe
+					first_iframe = 1
+
+					real_time_value = " " + real_time(pkt_dts_time, pts_offset)
 
 					# Zero out both the GOP and the count values
 					gop_line = ""
@@ -84,18 +100,16 @@ def extract_frames(probe_result):
 						gop_line += str(frame)
 						count += 1
 
+					frames = []
+					frames.append(pts)
+					frames.append(real_time_value)
+					frames.append(pict_type)
+					
 					if len(gop_line) > 1:
-						gop_result = str(count-1) + " " + gop_line
+						gop_result = str(count-2) + " " + gop_line
 						print(gop_result)
 						with open(text_file, 'a') as output:
 							output.write(gop_result+"\n")
-					
-					# Need to zero out the array each time so I-Frames start on new lines
-					# Zero the array, add the pts and the I-Frame
-					frames = []
-					frames.append(pts)
-					frames.append(pkt_dts_time)
-					frames.append(pict_type)
 				else:
 					# If the frame is not an I-Frame then tag it onto the end
 					frames.append(pict_type)
