77753b8694debe7be3e3fc7606574177797624c3
Richard Newton <therichardnewton+github@gmail.com>
2023-02-17 16:21:59 +0000

    get_manifest updated
    Updated to add some error handling.

--- get_manifest.sh
+++ get_manifest.sh
@@ -39,9 +39,26 @@ echo "Remeber to use ssh ppfe (or whatever) to make the connection to Traxis"
 echo "Gotta love VPN changes that break things..."
 
 details=$(curl -s "http://${server}:8443/traxis/web/Title/crid:~~2F~~2Fschange.com~~2F${provider_id}~~2F${resource_id}/Contents/Props/Aliases" | tr -d '\r')
+
+echo $details | grep -q ERestServcieResourceNotFound
+if [[ $? == 0 ]]
+then
+  echo "An error occurred: The file does not exist in Adrenalin."
+  exit 1
+fi
+
+echo $details | grep -q 'resultCount="0"'
+if [[ $? == 0 ]]
+then
+  echo "An error occurred: There are not assets with these attributes."
+  exit 1
+fi
+
 name=$(curl -s "http://${server}:8443/traxis/web/Title/crid:~~2F~~2Fschange.com~~2F${provider_id}~~2F${resource_id}/Props/Name" | grep Name | cut -d\> -f2 | cut -d\< -f1)
 backoffice_id=$(echo $details | grep VodBackOfficeId | cut -d\> -f6- | cut -d\< -f1)
-echo Fetching $name
+echo Fetching $name using a backoffice_id of: $backoffice_id
 #
 #http --body  --follow "http://${ms}/sdash/${backoffice_id}/index.mpd/Manifest?providerID=${provider_id}&assetID=${resource_id}&macid=${mac_id}&device=${device}" --output manifest.xml --download
 curl -s -o "${name}.xml" -L "http://${ms}/sdash/${backoffice_id}/index.mpd/Manifest?providerID=${provider_id}&assetID=${resource_id}&macid=${mac_id}&device=${device}"
+
+echo "File written to: ${name}.xml"
